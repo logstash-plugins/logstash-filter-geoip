b3b3c6a366aee502a610c05642db2d57a29ba122
Pier-Hugues Pellerin <phpellerin@gmail.com>
2014-11-18 18:35:34 -0500

    Plugin structure change

--- Rakefile
+++ Rakefile
@@ -6,3 +6,4 @@ task :default do
   system("rake -T")
 end
 
+require "logstash/devutils/rake"
--- spec/filters/geoip_spec.rb
+++ spec/filters/geoip_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "logstash/devutils/rspec/spec_helper"
 require "logstash/filters/geoip"
 
 describe LogStash::Filters::GeoIP do
