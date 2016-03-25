fe0062d44bbe518bca3301217f625eb827730efa
garyelephant <garygaowork@gmail.com>
2016-03-25 03:40:55 +0000

    remove docker related codes

--- lib/logstash-filter-geoip_jars.rb
+++ lib/logstash-filter-geoip_jars.rb
@@ -2,7 +2,7 @@
 require 'jar_dependencies'
 
 require_jar( 'com.fasterxml.jackson.core', 'jackson-databind', '2.6.4' )
-require_jar( 'com.maxmind.db', 'maxmind-db', '1.1.0' )
-require_jar( 'com.maxmind.geoip2', 'geoip2', '2.5.0' )
-require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.6.0' )
 require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.6.4' )
+require_jar( 'com.maxmind.geoip2', 'geoip2', '2.5.0' )
+require_jar( 'com.maxmind.db', 'maxmind-db', '1.1.0' )
+require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.6.0' )
