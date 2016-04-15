9ec88e975307dbc02e71ccc19136df2e18bf5dcd
garyelephant <garygaowork@gmail.com>
2016-04-15 02:45:25 +0000

    remove auto generated ruby file

--- lib/logstash-filter-geoip_jars.rb
+++
@@ -1,8 +0,0 @@
-# this is a generated file, to avoid over-writing it just delete this comment
-require 'jar_dependencies'
-
-require_jar( 'com.fasterxml.jackson.core', 'jackson-databind', '2.6.4' )
-require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.6.4' )
-require_jar( 'com.maxmind.geoip2', 'geoip2', '2.5.0' )
-require_jar( 'com.maxmind.db', 'maxmind-db', '1.1.0' )
-require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.6.0' )
